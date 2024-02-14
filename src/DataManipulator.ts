3cdf255e6831bfee0b40490a8bbbafc1f0b9e534
dragonemperor-ulti <117937903+dragonemperor-ulti@users.noreply.github.com>
2024-02-14 15:11:03 +0000

    in this task i have created a graph based on the ratio and not on individual stocks

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,36 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc : number,
+  price_def : number ,
+  ratio : number ,
+  timestamp : Date, 
+  upper_bound : number,
+  lower_bound : number,
+  trigger_alert : number | undefined,
+  
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    const price_ABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const price_DEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = price_ABC / price_DEF;
+    const upper_Bound = 1 +  0.1;
+    const lower_Bound = 1 -  0.1;
+
+    return {
+      price_abc : price_ABC,
+      price_def : price_DEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+        serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound : upper_Bound,
+      lower_bound : lower_Bound,
+      trigger_alert : (ratio > upper_Bound || ratio < lower_Bound) ? ratio : undefined,
+        
+    };
   }
 }
+
